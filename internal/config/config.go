6a5d845a76d4199d69a5144548d5535a0e682271
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2026-01-17 17:46:31 +0000

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,47 +3,72 @@ package config
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
-	"time"
 )
 
 type Config struct {
-	Mode string
+	MqttBrokerHost string
+	MqttBrokerPort string
+	MqttUsername   string
+	MqttPassword   string
+	MqttClientId   string
+
+	MqttDiscoveryPrefix string
+	MattStatePrefix     string
+
+	HaDeviceId           string
+	HaDeviceName         string
+	HaDeviceManufacturer string
+	HaDeviceModel        string
+
+	DataDir string
 }
 
 func Load() (Config, error) {
 	cfg := Config{}
 
-	cfg.Mode = getEnv("MODE", "dev")
+	var err error
+
+	cfg.MqttBrokerHost, err = getEnvRequired("MQTT_BROKER_HOST")
+	if err != nil {
+		return Config{}, err
+	}
+	cfg.MqttBrokerPort = getEnvDefault("MQTT_BROKER_PORT", "1883")
+	cfg.MqttUsername, err = getEnvRequired("MQTT_USERNAME")
+	if err != nil {
+		return Config{}, err
+	}
+	cfg.MqttPassword, err = getEnvRequired("MQTT_PASSWORD")
+	if err != nil {
+		return Config{}, err
+	}
+	cfg.MqttClientId = getEnvDefault("MQTT_CLIENT_ID", "go-mqtt-tester")
+
+	cfg.MqttDiscoveryPrefix = getEnvDefault("MQTT_DISCOVERY_PREFIX", "homeassistant")
+	cfg.MattStatePrefix = getEnvDefault("MQTT_STATE_PREFIX", "go-mqtt-tester")
+
+	cfg.HaDeviceId = getEnvDefault("HA_DEVICE_ID", "go-mqtt-tester")
+	cfg.HaDeviceName = getEnvDefault("HA_DEVICE_NAME", "Go MQTT Tester")
+	cfg.HaDeviceManufacturer = getEnvDefault("HA_DEVICE_MANUFACTURER", "Local Dev")
+	cfg.HaDeviceModel = getEnvDefault("HA_DEVICE_MODEL", "Fake IoT Device")
+
+	cfg.DataDir = getEnvDefault("DATA_DIR", "./data")
 
 	return cfg, nil
 }
 
-func getEnv(key, def string) string {
-	if val := os.Getenv(key); val != "" {
-		return val
-	}
-	return def
-}
-
-func parseIntEnv(key string, def int) (int, error) {
+func getEnvRequired(key string) (string, error) {
 	val := strings.TrimSpace(os.Getenv(key))
 	if val == "" {
-		return def, nil
+		return "", fmt.Errorf("configuration error: required environment variable %q is not set", key)
 	}
-	n, err := strconv.Atoi(val)
-	if err != nil {
-		return 0, fmt.Errorf("invalid %s: %w", key, err)
-	}
-	return n, nil
+	return val, nil
 }
 
-func parseDurationEnv(key, def string) (time.Duration, error) {
-	val := getEnv(key, def)
-	d, err := time.ParseDuration(val)
-	if err != nil {
-		return 0, fmt.Errorf("invalid %s: %w", key, err)
+func getEnvDefault(key, def string) string {
+	val := strings.TrimSpace(os.Getenv(key))
+	if val == "" {
+		return def
 	}
-	return d, nil
+	return val
 }
